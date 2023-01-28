31cf0517de92639cd14c8180a4b3bbd8023b9412
Carol Moreira <caahrolmoreira@gmail.com>
2023-01-28 17:20:16 -0300

    add products data

---
+++ src/books.js
@@ -0,0 +1,83 @@
+import LittlePlanets from "./images/books/8-little-planets.jpg"
+import ComoEOEspaco from "./images/books/como-e-o-espaco.jpg"
+import Espaco from "./images/books/espaco.jpg"
+import EstrelasEPlanetas from "./images/books/estrelas-e-planetas.jpg"
+import JujuAstronauta from "./images/books/juju-quer-ser-astronauta.jpg"
+import MeuPrimeiroL from "./images/books/meu-primeiro-livro-espaco.jpg"
+import OAstronauta from "./images/books/o-astronauta.jpg"
+import ExploreSistem from "./images/books/solar-sistem.jpg"
+import NerdBabies from "./images/books/nerd-babies.jpg"
+import PlanetsBook from "./images/books/livro-planetas.webp"
+
+export const books = [{
+  id: 1,
+  imagem: LittlePlanets,
+  titulo: "8 Little Planets",
+  valor: 47.80,
+  quantidade: 0
+
+},
+{
+  id: 2,
+  imagem: ComoEOEspaco,
+  titulo: "Como é o espaço?",
+  valor: 25.60,
+  quantidade: 0
+},
+{
+  id: 3,
+  imagem: Espaco,
+  titulo: "Espaço",
+  valor: 15.90,
+  quantidade: 0
+},
+{
+  id: 4,
+  imagem: EstrelasEPlanetas,
+  titulo: "Estrelas e Planetas",
+  valor: 23.60,
+  quantidade: 0
+},
+{
+  id: 5,
+  imagem: JujuAstronauta,
+  titulo: "Juju quer ser Astronauta",
+  valor: 31.20,
+  quantidade: 0
+},
+{
+  id: 6,
+  imagem: MeuPrimeiroL,
+  titulo: "Meu primeiro livro Espaço",
+  valor: 16.90,
+  quantidade: 0
+
+},
+{
+  id: 7,
+  imagem: OAstronauta,
+  titulo: "O Astronauta",
+  valor: 34.30,
+  quantidade: 0
+},
+{
+  id: 8,
+  imagem: ExploreSistem,
+  titulo: "Explorando o Sistema Solar",
+  valor: 62.50,
+  quantidade: 0  
+},
+{
+  id: 9,
+  imagem: NerdBabies,
+  titulo: "Nerd Babies - Space",
+  valor: 38.90,
+  quantidade: 0  
+},
+{
+  id: 10,
+  imagem: PlanetsBook,
+  titulo: "O grande livro dos Planetas",
+  valor: 41.80,
+  quantidade: 0  
+}]  
